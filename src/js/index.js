410ab1ef4deeb50e7be03ab4b8691dc7e9d4d67e
Gustavo Corrêa <156522530+gtrstx@users.noreply.github.com>
2024-05-17 13:31:57 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,95 @@
+var home = window.document.getElementById('menuhome')
+home.addEventListener('mouseenter', entrar1)
+home.addEventListener('mouseout', sair1)
+
+function entrar1() {
+home.style.color = '#F9EC88'
+}
+
+function sair1() {
+    home.style.color = 'white'
+}
+
+var blog = window.document.getElementById('menublog')
+blog.addEventListener('mouseenter', entrar2)
+blog.addEventListener('mouseout', sair2)
+
+function entrar2() {
+    blog.style.color = '#F9EC88'
+}
+
+function sair2() {
+    blog.style.color = 'white'
+}
+
+var forums = window.document.getElementById('menuforums')
+forums.addEventListener('mouseenter', entrar3)
+forums.addEventListener('mouseout', sair3)
+
+function entrar3() {
+    forums.style.color = '#F9EC88'
+}
+
+function sair3() {
+    forums.style.color = 'white'
+}
+
+var wiki = window.document.getElementById('menuwiki')
+wiki.addEventListener('mouseenter', entrar4)
+wiki.addEventListener('mouseout', sair4)
+
+function entrar4() {
+    wiki.style.color = '#F9EC88'
+}
+
+function sair4() {
+    wiki.style.color = 'white'
+}
+
+var media = window.document.getElementById('menumedia')
+media.addEventListener('mouseenter', entrar5)
+media.addEventListener('mouseout', sair5)
+
+function entrar5() {
+    media.style.color = '#F9EC88'
+}
+
+function sair5() {
+    media.style.color = 'white'
+}
+
+var merch = window.document.getElementById('menumerch')
+merch.addEventListener('mouseenter', entrar6)
+merch.addEventListener('mouseout', sair6)
+
+function entrar6() {
+    merch.style.color = '#F9EC88'
+}
+
+function sair6() {
+    merch.style.color = 'white'
+}
+
+var faq = window.document.getElementById('menufaq')
+faq.addEventListener('mouseenter', entrar7)
+faq.addEventListener('mouseout', sair7)
+
+function entrar7() {
+    faq.style.color = '#F9EC88'
+}
+
+function sair7() {
+    faq.style.color = 'white'
+}
+
+var tabletop = window.document.getElementById('menutabletop')
+tabletop.addEventListener('mouseenter', entrar8)
+tabletop.addEventListener('mouseout', sair8)
+
+function entrar8() {
+    tabletop.style.color = '#F9EC88'
+}
+
+function sair8() {
+    tabletop.style.color = 'white'
+}
